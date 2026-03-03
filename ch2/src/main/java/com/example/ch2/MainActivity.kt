2e0016120c446f6f6d56ef7510ff7d53a210cf12
juu124 <juu12424@gmail.com>
2026-03-03 11:31:36 +0900

    [ch2 : Firebase 실습] 1. addFab 버튼 클릭시 add 페이지 이동 로직 추가

--- ch2/src/main/java/com/example/ch2/MainActivity.kt
+++ ch2/src/main/java/com/example/ch2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,7 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-
+        binding.addFab.setOnClickListener {
+            if (MyApplication.checkAuth()) {
+                startActivity(Intent(this, AddActivity::class.java))
+            } else {
+                Toast.makeText(this, "인증을 먼저 진행해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onStart() {
