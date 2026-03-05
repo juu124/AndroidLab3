eaa552b24029caa25bb94c176f3b85e0695cf836
juu124 <juu12424@gmail.com>
2026-03-05 13:10:11 +0900

    [ch3 : Navigation Architecture Component 실습] 1. bundle로 fragment끼리 데이터 전달

--- ch3/src/main/java/com/example/ch3/section4/AFragment.kt
+++ ch3/src/main/java/com/example/ch3/section4/AFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.ch3.R
 import com.example.ch3.databinding.FragmentABinding
 
 class AFragment : Fragment() {
@@ -14,6 +16,21 @@ class AFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentABinding.inflate(inflater, container, false)
+
+        binding.button6.setOnClickListener {
+            val controller = Navigation.findNavController(it)
+            // nav graph 에 등록되어 있는 프래그먼트의 id로 화면전환 : action의 id로 이동
+//            controller.navigate(R.id.BFragment)
+
+            // 화면 전환시 데이터 전달
+            val bundle = Bundle()
+            bundle.putString("arg1", "hello")
+            bundle.putInt("arg2", 10)
+
+            // action의 id로 화면 전환
+            controller.navigate(R.id.action_AFragment_to_BFragment, bundle)
+        }
+
         return binding.root
     }
 }
--- ch3/src/main/java/com/example/ch3/section4/BFragment.kt
+++ ch3/src/main/java/com/example/ch3/section4/BFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.ch3.databinding.FragmentBBinding
 
 class BFragment : Fragment() {
@@ -14,6 +16,19 @@ class BFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentBBinding.inflate(inflater, container, false)
+
+        // 전달된 데이터 획득
+        val bundle = arguments
+        val arg1 = bundle?.getString("arg1")
+        val arg2 = bundle?.getInt("arg2")
+
+        Toast.makeText(activity, "$arg1, $arg2", Toast.LENGTH_SHORT).show()
+
+        binding.button6.setOnClickListener {
+            val controller = Navigation.findNavController(it)
+            controller.navigateUp()
+        }
+
         return binding.root
     }
 }
